9d94e269c4c139d0f82830d1aca7a7b214c09bf5
mipapo <michael@mipapo.de>
2015-07-23 21:26:47 +0200

    working controller login/app page

--- src/Runalyze/HomeController.php
+++ src/Runalyze/HomeController.php
@@ -3,6 +3,9 @@ namespace Runalyze;
  
 use Symfony\Component\HttpFoundation\Response;
 use Silex\Application;
+use \DB;
+use \Cache;
+
 function userStat() {
     DB::getInstance()->stopAddingAccountID();
     $stat['user'] = Cache::get('NumUser', 1);
@@ -16,9 +19,9 @@ function userStat() {
         $km= DB::getInstance()->query('SELECT SUM(distance) FROM '.PREFIX.'training')->fetchColumn();
         Cache::set('NumKm', $NumKm, '500', 1);
     }
-    $stat['km'] = Runalyze\Activity\Distance::format($km);
+    $stat['km'] = Activity\Distance::format($km);
     DB::getInstance()->startAddingAccountID();
-    $stat['online'] = SessionAccountHandler::getNumberOfUserOnline();
+    $stat['online'] = \SessionAccountHandler::getNumberOfUserOnline();
     return $stat;
 }
 function cookeInfo() {
@@ -28,39 +31,37 @@ function cookeInfo() {
 function switchStart() {
     if($_POST['new_username']) {
         $path = 'register';
-        $RegistrationErrors = AccountHandler::tryToRegisterNewUser();
+        $RegistrationErrors = \AccountHandler::tryToRegisterNewUser();
     } elseif($_POST['username'])
         $path = 'login';
     elseif($_POST['send_username']) {
         $path = 'forgotpw';    
-        $forgotpw =  AccountHandler::sendPasswordLinkTo($_POST['send_username']);
+        $forgotpw =   \AccountHandler::sendPasswordLinkTo($_POST['send_username']);
     } else
         $path = 'login';
 }
 class HomeController
 {
-    public function loginAction()
+    public function loginAction(Application $app)
     {
             $Frontend = new \Frontend(true);
                 $stat = userStat();
                 $cookieinfo = cookeInfo();
                 //Cooke information
                  $path = 'login';
-
-                $response = $app['twig']->render('login.twig', array(
+                $response =  $app['twig']->render('login.twig', array(
                     'RUNALYZE_VERSION' => RUNALYZE_VERSION,
                     'numUserOnline' => $stat['online'],
                     'numUser' => $stat['user'],
                     'numKm' => $stat['km'],
-                    'errorType' => SessionAccountHandler::$ErrorType,
+                    'errorType' => \SessionAccountHandler::$ErrorType,
                     'cookieInfo' => $cookieinfo,
                     'switchpath' => $path,
                     'forgotpw' => $forgotpw,
                     'USER_CAN_REGISTER' => USER_CAN_REGISTER,
                     'regError' => $RegistrationErrors,
                 ));
-                return new Respnse($response);
-        //return new Response("AppController::homeAction");
+                return new Response($response);
     }
  
     public function testAction()
@@ -77,7 +78,7 @@ class HomeController
                         <div id="data-browser" class="panel">
                                 <div id="data-browser-inner">
                                         <?php
-                                        $DataBrowser = new DataBrowser();
+                                        $DataBrowser = new \DataBrowser();
                                         $DataBrowser->display();
                                         ?>
                                 </div>
@@ -86,8 +87,8 @@ class HomeController
                         <div id="statistics" class="panel">
                                 <ul id="statistics-nav">
                                         <?php
-                                        $Factory = new PluginFactory();
-                                        $Stats = $Factory->activePlugins( PluginType::Stat );
+                                        $Factory = new \PluginFactory();
+                                        $Stats = $Factory->activePlugins( \PluginType::Stat );
                                         foreach ($Stats as $i => $key) {
                                                 $Plugin = $Factory->newInstance($key);
 
@@ -96,7 +97,7 @@ class HomeController
                                                 }
                                         }
 
-                                        if (PluginStat::hasVariousStats()) {
+                                        if (\PluginStat::hasVariousStats()) {
                                                 echo '<li class="with-submenu">';
                                                 echo '<a href="#">'.__('Miscellaneous').'</a>';
                                                 echo '<ul class="submenu">';
@@ -118,8 +119,8 @@ class HomeController
                                 <div id="statistics-inner">
                                         <?php
                                         if (isset($_GET['id'])) {
-                                                $Context = new Context(Request::sendId(), SessionAccountHandler::getId());
-                                                $View = new TrainingView($Context);
+                                                $Context = new \Context(Request::sendId(), \SessionAccountHandler::getId());
+                                                $View = new \TrainingView($Context);
                                                 $View->display();
                                         } elseif (isset($_GET['pluginid'])) {
                                                 $Factory->newInstanceFor((int)$_GET['pluginid'])->display();
